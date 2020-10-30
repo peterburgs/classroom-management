70febf930139970042225c7c6d921bd087c1bc7d
PeterBurgs <peterburgs.vn@gmail.com>
2020-10-30 09:16:00 +0700

    Config server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 const app = require("./app");
 
+// Config server
 const server = http.createServer(app);
 server.listen(port);
 
