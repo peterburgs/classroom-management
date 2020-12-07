93be4660b9efc303dab96cbeeebcf8443894e23d
PeterBurgs <peterburgs.vn@gmail.com>
2020-12-07 09:45:12 +0700

    Prepare for Schedule generation

---
+++ src/routes/scheduleRoutes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Import Models
+const User = mongoose.model("User");
+
+const router = express.Router();
+
+// POST Method Create user
+router.post("/", async (req, res) => {
+  const user = new User({
+    email: req.body.email,
+    fullname: req.body.fullname,
+    roles: req.body.role,
+  });
+  user
+    .save()
+    .then((doc) => {
+      res.status(201).json({
+        message: "Created",
+        user: doc,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Cannot created",
+        err,
+      });
+    });
+});
+
+// Export
+
+module.exports = router;
