99b2edbfba48f20d93507bb62006443d8e94ed8e
Peter Burgs <peterburgs.vn@gmail.com>
2020-12-21 00:03:53 +0700

    temporal commit

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -6,6 +6,53 @@ const Course = mongoose.model("Course");
 
 const router = express.Router();
 
+// GET Method: get all courses
+router.get("/", async (req, res) => {
+
+  try {
+    const result = await Course.find({}).exec()
+    if (result) {
+      console.log(result)
+      res.status(200).json({
+        message: "Found",
+        courses: result
+      })
+    }
+  }
+  catch (err) {
+    console.log(err.message)
+    res.status(404).json({
+      message: "Not Found",
+      error: err.message
+    })
+  }
+
+
+})
+
+// GET Method: get course by Id
+router.get("/:courseId", async (req, res) => {
+  const id = req.params.courseId
+  try {
+    const course = await Course.findOne({ _id: id })
+    if (course) {
+      console.log(course)
+      res.status(200).json({
+        message: "Found",
+        course
+      })
+    }
+  } catch (err) {
+    console.log(err.message)
+    res.status(404).json({
+      message: "Not found",
+      error: err.message
+    })
+  }
+
+})
+
+
 // POST Method Create a new Course
 router.post("/", async (req, res) => {
   const course = new Course({
@@ -36,12 +83,13 @@ router.put("/:courseId", async (req, res) => {
     updateOps[key] = val;
   }
   try {
-    const result = await Course.updateOne(
+    const result = await Course.findByIdAndUpdate(
       { _id: id },
-      { $set: updateOps }
+      { $set: updateOps },
+      { new: true }
     ).exec();
     if (result) {
-      res.status(201).json({ result });
+      res.status(201).json({ course: result });
     }
   } catch (err) {
     res.status(500).json({
